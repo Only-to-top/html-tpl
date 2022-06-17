993782b12423d25869eb13a4c65459f7131e8604
Only to top <rlytrue@gmail.com>
2022-06-17 16:34:10 +0300

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,13 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
     if (document.querySelector(".swiper-1x")) { };
 
     // Fancybox
-    Fancybox.bind("[data-fancybox]", {
-        animationEffect: 'fade',
-        animationDuration: 555,
+    const popup = Fancybox.bind("[data-fancybox]", {
         l10n: {
             CLOSE: "Закрыть",
             NEXT: "Далее",
             PREV: "Назад",
+        },
+        template: {
+            closeButton: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" tabindex="-1"><path d="M20 20L4 4m16 0L4 20"/></svg>',
+        },
+        closeButton: 'inside',
+        on: {
+            done: (fancybox) => {
+                const prev_fancybox = fancybox.lastFocus.previousElementSibling;
+
+                if (prev_fancybox.classList.contains('fancybox__container')) {
+                    prev_fancybox.Fancybox.close(); // close prev popup
+                }
+            },
         },
     });
 
--- mail.php
+++ mail.php
@@ -8,6 +8,7 @@ $admin_email  = trim($_POST["admin_email"]);
 $form_subject = trim($_POST["form_subject"]);
 
 foreach ($_POST as $key => $value) {
+    $value = html_entity_decode($value);
     if (is_array($value)) {
         $value = implode(", ", $value);
     }
